1bd0757442376bef1736aa1d948df20539843c30
Jayani Walpola <59353705+jayaniwalpola@users.noreply.github.com>
2020-05-20 11:52:48 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Employee
+    {
+        double travelAllowance, performanceincentive, MS;
+        public void salary(double BS,double NL,double YS)
+        {
+            if(NL<=2)
+            {
+                performanceincentive = BS * 0.2;
+            }
+            else
+            {
+                performanceincentive = BS * 0.05;
+            }
+
+            if(YS>5)
+            {
+                travelAllowance = 10000;
+
+            }
+            else if(YS>2)
+            {
+                travelAllowance = 5000;
+
+            }
+            else
+            {
+                travelAllowance = 2500;
+            }
+            MS = BS + performanceincentive + travelAllowance;
+            Console.WriteLine("Congrats your monthly salary is " + MS);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BS, YofS, NofL;
+            Console.WriteLine("Enter employee Basic Salary:");
+            BS = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter years of service:");
+            YofS = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number of leaves:");
+            NofL = double.Parse(Console.ReadLine());
+            Employee emp1 = new Employee();
+            emp1.salary(BS, NofL, YofS);
+            Console.ReadLine();
+            
+        }
+    }
+}
